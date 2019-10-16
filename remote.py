f64192aac32a2d5ca6983bb39bfb2e6eb5c0aeb4
Christof Schlund <christof.schlund@iis.fraunhofer.de>
2019-10-16 16:36:37 +0200

    remote-Funktion in eigene Datei ausgelagert

---
+++ remote.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# This Python file uses the following encoding: utf-8
+
+
+import socket
+import sys
+import json
+import select
+import logging
+
+ADDR = "heizungeg.local"
+PORT = 5005
+udpTimeout = 4
+
+def tcpRemote(msg, **kwargs):
+    # Todo
+    pass
+
+def udpRemote(msg, **kwargs):
+    # Setzt einen JSON-String in Richtung Ampi per UDP ab
+    # msg: JSON-String nach Ampi-Spec
+    # udpSocket: wenn da ein Socket übergeben wird, wird halt der hergenommen
+    # addr, port: Gibt's keinen Socket, wird einer mit addr, port aufgemacht
+    # Wenn nix übergeben wird, gibt's halt einen Standard-Socket
+
+    if('udpSocket' in kwargs):
+        udpSocket = kwargs.get('udpSocket')
+    else:
+        if('addr' not in kwargs or 'port' not in kwargs):
+            logging.info("Uiui, wohin soll ich mich nur verbinden? Naja, standard halt.")
+            addr = ADDR
+            port = PORT
+        else:
+            addr = kwargs.get('addr')
+            port = kwargs.get('port')
+        logging.info("Öffne Socket")
+        try:
+            udpSocket = socket.socket( socket.AF_INET,  socket.SOCK_DGRAM )
+            udpSocket.setblocking(0)
+        except Exception as e:
+            logging.info(str(e))
+    try:
+        ret = -1
+        logging.info("So laut des heit: %s", msg)
+        ready = select.select([], [udpSocket], [], udpTimeout)
+        if(ready[1]):
+            udpSocket.sendto( msg.encode(), (addr,port) )
+            logging.info("Gesendet")
+        ready = select.select([udpSocket], [], [], udpTimeout)
+        if(ready[0]):
+            data, addr = udpSocket.recvfrom(1024)
+            logging.info(data.decode())
+            ret = data.decode()
+            return(ret)
+    except Exception as e:
+        logging.info(str(e))
+        return -1
+
+
+
+if __name__ == "__main__":
+     pass
--- remoteAmpi.py
+++ remoteAmpi.py
@@ -5,6 +5,7 @@ import sys
 import json
 import select
 import logging
+from remote import udpRemote
 
 
 logging.basicConfig(level=logging.INFO)
@@ -68,54 +69,8 @@ def getch():
     return ch
 
 
-def tcpRemote(msg, **kwargs):
-    # Todo
-    pass
-
-
-def udpRemote(msg, **kwargs):
-    # Setzt einen JSON-String in Richtung Ampi per UDP ab
-    # msg: JSON-String nach Ampi-Spec
-    # udpSocket: wenn da ein Socket übergeben wird, wird halt der hergenommen
-    # addr, port: Gibt's keinen Socket, wird einer mit addr, port aufgemacht
-    # Wenn nix übergeben wird, gibt's halt einen Standard-Socket
-
-    if('udpSocket' in kwargs):
-        udpSocket = kwargs.get('udpSocket')
-    else:
-        if('addr' not in kwargs or 'port' not in kwargs):
-            logging.info("Uiui, wohin soll ich mich nur verbinden? Naja, standard halt.")
-            addr = ADDR
-            port = PORT
-        else:
-            addr = kwargs.get('addr')
-            port = kwargs.get('port')
-        logging.info("Öffne Socket")
-        try:
-            udpSocket = socket.socket( socket.AF_INET,  socket.SOCK_DGRAM )
-            udpSocket.setblocking(0)
-        except Exception as e:
-            logging.info(str(e))
-    valid_cmds = getcmds()
-    try:
-        ret = -1
-        logging.info("So laut des heit: %s", msg)
-        ready = select.select([], [udpSocket], [], udpTimeout)
-        if(ready[1]):
-            udpSocket.sendto( msg.encode(), (addr,port) )
-            logging.info("Gesendet")
-        ready = select.select([udpSocket], [], [], udpTimeout)
-        if(ready[0]):
-            data, addr = udpSocket.recvfrom(1024)
-            logging.info(data.decode())
-            ret = data.decode()
-            return(ret)
-    except Exception as e:
-        logging.info(str(e))
-        return -1
-
 def main():
-    addr = 'osmd.fritz.box'
+    addr = 'osmd.local'
     port = 5005
 
     valid_cmds = getcmds()
