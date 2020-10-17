5ae475d3f11fa52c3240a74649d7fb9b024f8668
Christof <github@burningwheels.com>
2020-10-17 22:01:43 +0200

    Changed log level for some outputs

--- remote.py
+++ remote.py
@@ -33,7 +33,7 @@ def udpRemote(msg, **kwargs):
         else:
             addr = kwargs.get('addr')
             port = kwargs.get('port')
-        logging.info("Öffne Socket")
+        logging.debug("Öffne Socket")
         try:
             udpSocket = socket.socket( socket.AF_INET,  socket.SOCK_DGRAM )
             udpSocket.setblocking(0)
@@ -41,15 +41,15 @@ def udpRemote(msg, **kwargs):
             logging.info(str(e))
     try:
         ret = -1
-        logging.info("So laut des heit: %s", msg)
+        logging.debug("So laut des heit: %s", msg)
         ready = select.select([], [udpSocket], [], udpTimeout)
         if(ready[1]):
             udpSocket.sendto( msg.encode(), (addr,port) )
-            logging.info("Gesendet")
+            logging.debug("Gesendet")
         ready = select.select([udpSocket], [], [], udpTimeout)
         if(ready[0]):
             data, addr = udpSocket.recvfrom(8192)
-            logging.info(data.decode())
+            logging.debug(data.decode())
             ret = json.loads(data.decode())
             return(ret)
     except Exception as e:
