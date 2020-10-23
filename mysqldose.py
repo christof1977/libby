caa2c84f4fbd9b2d9d548e5ef19a9174e9f1431f
christof1977 <github@burningwheels.com>
2020-10-23 08:14:24 +0200

    Changed to one db connection per transaction, normalized database. write is ok, clac_pwr_month/year also. Besides that: still some work to do

--- mysqldose.py
+++ mysqldose.py
@@ -5,8 +5,8 @@ import pymysql
 import time
 
 import logging
-logging.basicConfig(level=logging.INFO)
-#logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 class mysqldose(object):
     def __init__(self, mysqluser, mysqlpass, mysqlserv, mysqldb):
@@ -14,97 +14,146 @@ class mysqldose(object):
         self.mysqlpass = mysqlpass
         self.mysqlserv = mysqlserv
         self.mysqldb = mysqldb
-        self.start()
-
-    def __del__(self):
-        self.close()
-
-    def start(self):
-        self.mysql_success = False
-        try:
-            #self.cnx = MySQLdb.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
-            self.cnx = pymysql.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
-            self.mysql_success = True
-            logging.info("Database connection established")
-        except Exception as e:
-            try:
-                self.mysql_success = False
-                logging.error("Database connection error: "+str(e))
-                print("1: ",str(e))
-                self.cnx.disconnect()
-            except Exception as e:
-                print("2: ",str(e))
-                pass
-
-    def close(self):
-        if self.mysql_success == True:
-            self.cnx.close()
-            logging.info("DB Connection terminated")
 
     def read_one(self, parameter, datetime = None):
         # reads a single value of the messwert table
         # parameter is the parameter to read
         # datetime can be empty or specified like "YYYY-MM-DD HH:mm:ss"
-        if self.mysql_success == True:
-            try:
-                self.cursor = self.cnx.cursor()
+        logging.debug("Opening connection to DB")
+        con = pymysql.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
+        try:
+            with con.cursor() as cur:
+                logging.debug("Executing query")
                 if datetime == None:
-                    query ='SELECT value FROM messwert WHERE parameter = %s ORDER BY `index` DESC LIMIT 0, 1;'
-                    result = self.cursor.execute(query, (parameter,))
+                    query ='SELECT messwert.value \
+                            FROM parameter \
+                            JOIN messwert \
+                            ON messwert.parameter = parameter.parid \
+                            WHERE parameter.parameter = %s \
+                            ORDER BY `messwert.index` DESC LIMIT 0, 1;'
+                    cur.execute(query, (parameter,))
                 else:
-                    query ='SELECT value FROM messwert WHERE parameter = %s AND datetime = %s;'
-                    result = self.cursor.execute(query, (parameter, datetime))
-                row = self.cursor.fetchall()
-                self.cnx.commit()
-                self.cursor.close()
-                #print(result)
-                return row[0][0]
-            except Exception as e:
-            #except (AttributeError, MySQLdb.OperationalError):
-                logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
-                self.mysql_success = False
-        else:
-            self.start()
+                    query ='SELECT messwert.value, messwert.datetime \
+                            FROM parameter \
+                            JOIN messwert ON messwert.parameter = parameter.parid \
+                            WHERE parameter.parameter = %s \
+                            AND messwert.datetime LIKE %s;'
+                    cur.execute(query, (parameter, datetime))
+                row = cur.fetchall()
+        except Exception as e:
+            logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
+            self.mysql_success = False
+        finally:
+            logging.debug("Closing connection to DB")
+            con.close()
+        return row[0][0]
 
     def read_many(self, parameter, datetime):
         # reads many values of the messwert table
         # parameter is the parameter to read
-        # datetime is speciefied as "2018-10-16%" for a whole day as example
-        if self.mysql_success == True:
-            try:
-                self.cursor = self.cnx.cursor()
-                query ='SELECT value, datetime FROM messwert WHERE parameter = %s AND datetime LIKE %s;'
-                result = self.cursor.execute(query, (parameter, datetime))
-                result = self.cursor.fetchall()
-                self.cnx.commit()
-                self.cursor.close()
+        # datetime is specified as "2018-10-16%" for a whole day as example
+        logging.debug("Opening connection to DB")
+        con = pymysql.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
+        try:
+            with con.cursor() as cur:
+                logging.debug("Executing query")
+                query ='SELECT value, datetime FROM messwert WHERE parameter = %s AND datetime LIKE %s LIMIT 10;'
+                print(query % (parameter, datetime))
+                cur.execute(query, (parameter, datetime))
+                logging.debug("Executing commit")
+                con.commit()
+                logging.debug("Executing fetchall")
+                result = cur.fetchall()
+                logging.debug(result)
+                #self.cnx.commit()
+                #self.cursor.close()
                 values = [elt[0] for elt in result]
                 datetimes = [elt[1] for elt in result]
                 #print(datetimes)
-                return values, datetimes
-            except Exception as e:
+        except Exception as e:
             #except (AttributeError, MySQLdb.OperationalError):
-                logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
-        else:
-            self.start()
+            logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
+        finally:
+            logging.debug("Closing connection to DB")
+            con.close()
+        return values, datetimes
 
-    def calc_pwr_h(self, datetime):
+    def calc_pwr(self, year, month=None, day=None, hour=None):
         # Calculates the collected solar power within a given hour
         # datetime hast to specified as follows: "2018-09-11 11%"
-        if self.mysql_success == True:
-            try:
-                self.cursor = self.cnx.cursor()
-                query ='SELECT ROUND(SUM(value)/COUNT(value), 3) FROM messwert WHERE datetime LIKE %s AND parameter = %s;'
-                result = self.cursor.execute(query, (datetime, "OekoKollLeistung"))
-                row = self.cursor.fetchall()
-                self.cnx.commit()
-                self.cursor.close()
-                return row[0][0]
-            except Exception as e:
-            #except (AttributeError, MySQLdb.OperationalError):
-                logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
-        else:
-            self.start()
+        con = pymysql.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
+        if(month is None):
+            start = str(year) + "-01-01"
+            end = str(year + 1) + "-01-01"
+        elif(day is None):
+            if(month < 1 or month > 12):
+                return
+            else:
+                start = str(year) + "-" + str(month) + "-01"
+            if(month == 12):
+                end = str(year + 1) + "-01-01" 
+            else:
+                end = str(year) + "-" + str(month + 1) + "-01" 
+        elif(hour is None):
+            if(day < 1 or day > 31):
+                return
+            else:
+                start = str(year) + "-" + str(month) + "-" + str(day)
+                end = str(year) + "-" + str(month) + "-" + str(day + 1)
+
+
+       # if(hour is None):
+       #     end_month = month + 1
+       #     if(end_month > 12):
+       #         end
+       #     start = str(year) + "-" + str(month)
+       #     end = str(year) + "-" + str(month + 1)
+
+        #start = day + " " + str(hour)
+        #end = day + " " + str(hour + 1)
+        print(start)
+        print(end)
+        #try:
+        #    with con.cursor() as cur:
+        #        query ='SELECT ROUND(SUM(value)/COUNT(value), 3) FROM messwert WHERE parameter = %s AND datetime BETWEEN %s AND %s'
+        #        cur.execute(query, ("OekoKollLeistung", start, end))
+        #        row = cur.fetchall()
+        #        con.commit()
+        #except Exception as e:
+        #    logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
+        #finally:
+        #    con.close()
+        #return row[0][0]
+
+    def calc_pwr_month(self, year, month):
+        con = pymysql.connect(user=self.mysqluser, passwd=self.mysqlpass,host=self.mysqlserv,db=self.mysqldb)
+        start = str(year) + "-" + str(month) + "-01"
+        try:
+            with con.cursor() as cur:
+                query = "SELECT \
+                        ROUND(SUM(value)/(3600/(86400/COUNT(value))) * \
+                        DATEDIFF(DATE_ADD(%s, INTERVAL 1 MONTH), %s), 3) \
+                        FROM messwert WHERE parameter = %s AND \
+                        datetime BETWEEN %s AND \
+                        DATE_ADD(%s, INTERVAL 1 MONTH);"
+                #query ='SELECT ROUND(SUM(value)/COUNT(value), 3) FROM messwert WHERE parameter = %s AND datetime BETWEEN %s AND DATE_ADD(%s, INTERVAL 1 MONTH)'
+                cur.execute(query, (start, start, "OekoKollLeistung", start, start))
+                row = cur.fetchall()
+                con.commit()
+        except Exception as e:
+            logging.error("Fehler beim lesen aus der Datenbank: "+str(e))
+        finally:
+            con.close()
+        return row[0][0]
+
+    def calc_pwr_year(self, year):
+        power = []
+        for month in range(1,13):
+            result = self.calc_pwr_month(year, month)
+            if(result is not None):
+                power.append(result)
+            print(month, ":",result, "kWh")
+        return(sum(power))
 
     def write(self, now, parameter, value):
         # writes to the messwert table of the database
@@ -118,29 +167,40 @@ class mysqldose(object):
         add = ("INSERT INTO messwert "
                 "(datetime, parameter, value) "
                 "VALUES (%s, %s, %s)")
-        data = (now, parameter, value)
-        logging.debug(data)
+        paridQ = "SELECT parid from parameter WHERE parameter = %s;"
         try:
             with con.cursor() as cur:
-                logging.debug("Executing query")
-                cur.execute(add, data)
+                cur.execute(paridQ, (parameter))
+                parid = cur.fetchall()
+                if(len(parid) == 0):
+                    insParQ = ("INSERT INTO parameter (parameter) \
+                               VALUES (%s);")
+                    res = cur.execute(insParQ, (parameter))
+                    con.commit()
+                    cur.execute(paridQ, (parameter))
+                    parid = cur.fetchall()
+                parid = parid[0][0]
                 mess_id = cur.lastrowid
+                con.commit()
+                logging.debug("Executing query")
+                data = (now, parid, value)
+                logging.debug(data)
+                res = cur.execute(add, data)
                 con.commit()
         except Exception as e:
             logging.error("Fehler beim Schreiben in die Datenbank: "+str(e))
-            self.close()
-            self.start()
         finally:
             logging.debug("Closing connection to DB")
             con.close()
         return mess_id
 
 if __name__ == "__main__":
-    dbconn = mysqldose('heizung', 'heizung', 'dose.fritz.box', 'heizung')
-    dbconn.start()
+    dbconn = mysqldose('heizung', 'heizung', 'dose', 'heizung')
     #dbconn.write('2017-11-12 1:2:3', 'Test', 44.0)
-    #result = dbconn.read_one("OekoKollLeistung", "2018-09-12 18:42:25")
-    #result = dbconn.calc_pwr_h("2018-09-12 17%")
-    #print(result)
-    dbconn.read_many("OekoKollLeistung", "2018-10-16%")
-    dbconn.close()
+    result = dbconn.read_one("OekoKollLeistung", "2018-09-12")
+    print(result)
+    #result = dbconn.calc_pwr("2020-09", day= 12, hour=12)
+    #result = dbconn.calc_pwr(2019, month=11, day = 3)
+    #print(dbconn.read_many("OekoAussenTemp", "2020-10-18 18:01%"))
+    #print(dbconn.read_one("OekoAussenTemp"))
+    #print(dbconn.calc_pwr_year(2020), "kWh")
